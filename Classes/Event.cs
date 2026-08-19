dd5ba15d0ae104288da31d5a66ae24775593ac1d
Dina N. Sakæussen <disa5@student.eadania.dk>
2026-08-19 15:52:01 +0200

    kommenteret koden

--- Classes/Event.cs
+++ Classes/Event.cs
@@ -5,17 +5,32 @@ namespace _2.semEksamenProjekt
 {
     public class Event
     {
-        public int id;
-        public string title = "";
+        public int id; //disse er helt almindelige fields, med deklaration direkte i klassen (og heller ingen get; set; accessors som ved properties) 
+        public string title = ""; //field med default "tom" værdi
         public string description = "";
         public List<string> rooms;
         public DateTime start;
         public DateTime end;
         public Flow flowLink;
         public List<User> teachers;
-        public string city = String.Empty;
-        public List<Team> teams = new List<Team>();
-        public List<string> tags = new List<string>();
+        public string city = String.Empty; //field deklareret/initialized med en default tom string
+        public List<Team> teams = new List<Team>(); //field deklareret med default tomme parametre()
+        // FYLDESTGØRENDE FORKLARING: public List<string> tags = new List<string>();
+        // Access modifier er valgt som "public", som derfor kan læses/skrives fra udenfor klassen.
+        // "List<string>" er typen af variablen.
+        // Da "<string>" er udfyldt med "string" og ikke den generiske "T", så skal denne liste specifikt indeholde string-værdier
+        // "tags" er et selvvalgt variabelnavn.
+        // "=" er assigment operatoren, og hvadend der er på højre side, bliver tillagt "tags".
+        // "new List<string>()" er selve objektoprettelsen,
+        // "new" allokerer et nyt List<string> objekt i hukommelsen.
+        // "()" kalder constructoren (i dette tilfælde parameterløse constructor, som derfor opretter en tom liste)
+
+        // KORT FORKLARING: public List<string> tags = new List<string>(); 
+        // Opretter en PUBLIC FIELD kaldet TAGS, 
+        // som er en LIST der indeholder STRING, 
+        // der INITIALISERES for nu, som en ny (tom) liste
+        public List<string> tags = new List<string>(); // I tilfælde af, at det i stedet for field var PROPERTY, 
+        // havde det været: List<string> tags { get; set; }
 
         // tilføjer event til event overviewet
         public void AddEvent(EventOverview overview)
